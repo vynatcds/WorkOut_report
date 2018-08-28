798f844f455349422278a3efd1341ba4fd73afd9
Nattanon <vynatcds@gmail.com>
2018-08-28 10:07:24 +0700

    edit index for test

--- src/index.js
+++ src/index.js
@@ -1,18 +1,16 @@
 import React from 'react';
-import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom';
-import { createStore,applyMiddleware } from 'redux';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-import reducers from './reducers';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
 
+import App from './components/app';
+import reducers from './reducers';
+//comment
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
-    <Provider store={createStoreWithMiddleware(reducers)}>
-        <App/>
-    </Provider>
-    , document.getElementById('root'));
-registerServiceWorker();
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <App />
+  </Provider>
+  , document.querySelector('.container'));
